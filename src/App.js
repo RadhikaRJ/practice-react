303982d8c3c103d2c7ea756fd79baf7b2ed4d0c1
RadhikaRJ <radhika7002@gmail.com>
2021-02-17 16:35:12 +0530

    alphanumeric Password Match exercise Implemented

--- src/App.js
+++ src/App.js
@@ -70,8 +70,8 @@ function App() {
         return latestPasswordTwoValue;
       });
 
-      var latestPasswordValue=event.target.value;
-      if(passwordOne!=latestPasswordValue){
+      
+      if(passwordOne!=updatedPasswordTwo){
         setPasswordMatchStatus(current=>"passwords are not matching");
        
       }
@@ -82,16 +82,74 @@ function App() {
    }
 
   //Alphanumeric password---------------------------------------------------------------------------------------
+   const [alphanumericPaswdOne,setAlphanumericPswdOne] = useState("");
+   const [alphanumericPaswdTwo,setAlphanumericPswdTwo] = useState("");
+   const [alphanumericPasswordMatchStatus, setAlphanumericPasswordMatchStatus] = useState("");
+   const [isAlphanumeric,setisAlphanumeric] = useState("");
 
-   
+   function handleAlphanumericPswdOne(event){
+      var alpha_pswd_one=event.target.value;
+      setAlphanumericPswdOne(currentPassword=>{
+        const latestPswdOne=alpha_pswd_one;
+        return latestPswdOne;
+      });
+      var hasNumFlag=0;
+      for(var i=0;i<alpha_pswd_one.length;i++){
+        var letter=alpha_pswd_one.charAt(i);
+        if(!isNaN(letter)){
+         hasNumFlag+=1;
+        }
+      }
+      if(hasNumFlag==0){
+        setisAlphanumeric(currentStatus=>{
+          const status="Not Alphanumeric";
+          return status;
+        });
+      }
+      else{
+        setisAlphanumeric(currentStatus=>{
+          const status="Is Alphanumeric";
+          return status;
+        })
+      }
+   }
+
+
+   function compareAlphanumericPasswords(event){
+     var updatedAlphanumericPswdTwo=event.target.value;
+     setAlphanumericPswdTwo(updatePswdTwo=>{
+       const pswdtwo=updatedAlphanumericPswdTwo;
+       return pswdtwo;
+     });
+
+     if(alphanumericPaswdOne!=updatedAlphanumericPswdTwo){
+      setAlphanumericPasswordMatchStatus(status=>"Passwords are not Matching!");
+     }
+     else {
+      setAlphanumericPasswordMatchStatus(status=>"Passwords are Matching!");
+     }
+   }
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
+
+
       <h3>A simple counter</h3>
       <p> <button onClick={incrementValue}>+</button>
         <span>    {counter}    </span>
         <button onClick={decrementValue}>-</button>
         </p>
        
+    <hr></hr>
+
 
         <h3>Character counter -Twitter</h3>
        <input type="text" 
@@ -99,6 +157,8 @@ function App() {
        ></input>
         <p>{userInput}</p>
         <p>{currentLength} is your current char count<br></br>Chars left: {charLeftLength}</p>
+
+<hr></hr>
 
 
        <h3>Password Match</h3>
@@ -108,17 +168,32 @@ function App() {
        onChange={handleFirstPasswordInput}></input>
        <br></br>
        <p>Password one is: {passwordOne}</p>
+       
 
        <label>Confirm password: </label>
        <input type="password"
        onChange={comparePasswords}></input>
        <br></br>
-       <p>Password two is: {passwordTwo}</p>
-       
-     
+       <p>Password two is: {passwordTwo}</p>     
        <p>You password match result is: {passwordMatchStatus}</p>
 
+      <hr></hr>
 
+
+      <h3>Alphanumeric password</h3>
+      <label>Enter Alphanumeric Password: </label>
+      <input type="password"
+      onChange={handleAlphanumericPswdOne}></input>
+      <p>Password entered: {alphanumericPaswdOne}</p>
+      <p>Alphanumeric check result: {isAlphanumeric}</p>
+
+      <label>Confirm Password: </label>
+      <input type="password" 
+      onChange={compareAlphanumericPasswords}></input>
+      <p>Password entered: {alphanumericPaswdTwo}</p>
+      <p>Password Match result: {alphanumericPasswordMatchStatus} </p>
+
+      <hr></hr>
     </div>
   );
 }
